ed0eba825e060a80b1b310c673888c87dacc2220
“williampalacios” <mich.palacios.hdz@gmail.com>
2019-11-13 16:34:25 -0600

    Templates modificados para mostrar post desde base de datos.

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def prod_list(request):
-    return render(request, 'catalogo/prod_list.html', {})
+    productos = Producto.objects.order_by('nombre')
+    return render(request, 'catalogo/prod_list.html', {'prods': productos})
