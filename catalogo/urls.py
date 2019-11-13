eecd43d641f29dd3377a7163f41f4d2336d04552
“williampalacios” <mich.palacios.hdz@gmail.com>
2019-11-13 12:02:01 -0600

    Tienda Virtual Test, commit primer template

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
-
+    path('', views.prod_list, name='prod_list'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def prod_list(request):
+    return render(request, 'catalogo/prod_list.html', {})
